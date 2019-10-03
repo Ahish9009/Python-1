64a0f3b556f15ddad958f28cfb4ed4268a0b57b2
Ahish <ahishjayant9009@gmail.com>
2019-10-04 01:24:22 +0530

    Update FindMax.py

--- Maths/FindMax.py
+++ Maths/FindMax.py
@@ -1,11 +1,11 @@
 # NguyenU
 
 def find_max(nums):
-    max = nums[0]
+    maxNum = nums[0]
     for x in nums:
-      if x > max:
-        max = x
-    print(max)
+      if x > maxNum:
+        maxNum = x
+    print(maxNum)
 
 def main():
   find_max([2, 4, 9, 7, 19, 94, 5])
